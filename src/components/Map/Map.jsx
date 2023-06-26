6d1ad64a493554dbb5990271b0720f160dcd0bad
she1018 <she020108@gmail.com>
2023-06-27 01:43:42 +0900

    :sparkles: 전통시장 위치 마커 추가 (Map.jsx)
    현재 MarketMarker에서 API 데이터 받아오고 Map에서 마커 추가함. 마커 추가 기능 MarketMarker 컴포넌트로 옮겨야 함.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, } from 'react';
+import React, { useState, useEffect, useRef, } from 'react';
 import { styled } from "styled-components"
 import MarketMarker from "./MarketMarker";
 
 const Map = () => {
+  const [data, setData] = useState();
   const mapElement = useRef(null);
+  const { naver } = window;
 
   useEffect(() => {
     const { naver } = window;
@@ -31,10 +33,41 @@ const Map = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (!mapElement.current || !naver) return;
+
+    // 지도 렌더링 시, 최초 장소 표시
+    // 영천시장 좌표: 37.570690, 126.961031
+    const location = new naver.maps.LatLng(37.570690, 126.961031);
+
+    // 네이버 지도 옵션 선택
+    const mapOptions = {
+      center: location,
+      zoom: 16,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_LEFT,
+      },
+    };
+
+    const naverMap = new naver.maps.Map(mapElement.current, mapOptions);
+
+    if (data) {
+      data.forEach(item => {
+        new naver.maps.Marker({
+          position: new naver.maps.LatLng(item.latitude, item.longitude),
+          map: naverMap,
+        });
+      });
+    };
+
+  }, [data]);
+
+
   return (
     <Wrapper>
       <MapContainer ref={mapElement}/>
-      <MarketMarker />
+      <MarketMarker data={data} setData={setData}/>
     </Wrapper>
   );
 };
@@ -44,6 +77,7 @@ export default Map;
 // styled
 const Wrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
--- src/components/Map/MarketMarker.jsx
+++ src/components/Map/MarketMarker.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { styled } from "styled-components";
 import axios from "axios";
 
-const MarketMarker = () => {
+const MarketMarker = ( {data, setData} ) => {
   const MARKET_KEY = process.env.REACT_APP_MARKET_ID;
   const MARKET_API_URL = `https://api.odcloud.kr/api/15052836/v1/uddi:2253111c-b6f3-45ad-9d66-924fd92dabd7?serviceKey=${MARKET_KEY}&page=1&perPage=1439&returnType=json`
   const MARKET_STANDARD_API_URL = `http://api.data.go.kr/openapi/tn_pubr_public_trdit_mrkt_api?serviceKey=${MARKET_KEY}&pageNo=1&numOfRows=100&type=json`;
+
   const loadData = async () => {
     try {
       await axios
@@ -13,6 +14,11 @@ const MarketMarker = () => {
         .then((res) => {
           const jsonRes = res.data;
           console.log(jsonRes);
+          return jsonRes.response.body.items;
+        })
+        .then(data => {
+          console.log(data);
+          setData(data);
         })
     } catch (error) {
       console.log(error);
@@ -20,7 +26,7 @@ const MarketMarker = () => {
   };
 
   useEffect(() => {
-    loadData();
+    if (!data) loadData();
   }, [loadData]);
 
   return (
