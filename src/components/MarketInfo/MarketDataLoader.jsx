555c7e7d31ed1e4de390d168bdb3efd91336acef
she1018 <she020108@gmail.com>
2023-06-28 18:40:01 +0900

    :art: MarketImageLoader 구현 완료

--- src/components/MarketInfo/MarketDataLoader.jsx
+++ src/components/MarketInfo/MarketDataLoader.jsx
@@ -1,18 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
-import marketsLocation from "../../markets.json";
 
-/*
-시장명 기준으로 데이터 가져오려고 했는데 시장명 중복 있음...
-공공데이터 불러올 땐 그냥 중복되는 것까지 불러와도 큰 문제는 없음
-파베에서는 시장명 key로 쓰지 말고 key값 별도로 부여해야 할 듯 (0000~1520)
-마커에도 클릭이벤트핸들러 추가하려면 key 필요할 것 같은데.. 아예 markets.json에 넣을까
-  markers는 배열의 index가 key(0~1520). But loadFirst/loadLater을 시장명으로 불러오면 순서가 바뀌니까 대응하는 marker에 전달 불가능
-  markets.json에 key 추가하고, filter할 때 marketsLocation에서 {key: market.key, marketName: market.mrktNm}를 loadFirst/loadLater에 push
-  그러면 loadData에서 불러온 데이터 어딘가에 저장할 때 key도 같이 저장되고, marker 이벤트 추가할 때 사용 가능
-  (파베에서 불러오는 데이터(사진 등)도 같은 key값으로 불러올 수 있음)
-=> 일단 markets.json에 key 추가할 것
-*/
 
 const MarketDataLoader = ({ marketIndex, setMarketData, setDataLoaded }) => {
   const MARKET_KEY = process.env.REACT_APP_MARKET_ID;
--- src/components/MarketInfo/MarketImageLoader.jsx
+++ src/components/MarketInfo/MarketImageLoader.jsx
@@ -7,27 +7,53 @@ images/markets/${marketIndex}/
 */
 
 const MarketImageLoader = ({ marketIndex, setImageUrl }) => {
-    const imageRef = ref(firebaseStorage, `images/markets/${marketIndex}/market_${marketIndex}.jpg`);
 
-    // listAll(imageRef)
-    //     .then((res) => {
-    //         console.log(res.items);
-    //     }).catch((error) => {
+    //1. 파베에 이미지 파일명 market_${marketIndex}.jpg로 등록하는 경우
+
+    // const imageRef = ref(firebaseStorage, `images/markets/${marketIndex}/market_${marketIndex}.jpg`);
+
+    // getDownloadURL(imageRef)
+    //     .then((url) => {
+    //         // `url` is the download URL for `images/markets/${marketIndex}/market_${marketIndex}.jpg`
+
+    //         // Or inserted into an <img> element
+    //         setImageUrl(url);
+    //     })
+    //     .catch((error) => {
     //         console.log(error);
+    //         // Handle any errors
     //     });
 
 
-    getDownloadURL(imageRef)
-        .then((url) => {
-            // `url` is the download URL for `images/markets/${marketIndex}/market_${marketIndex}.jpg`
 
-            // Or inserted into an <img> element
-            setImageUrl(url);
+    //2. 그냥 아무 파일명으로 업로드할 경우
+
+    const imageRef = ref(firebaseStorage, `images/markets/${marketIndex}`);
+
+    listAll(imageRef)
+        .then((res) => {
+            return res.items;
+        })
+        .then((list) => {
+            if (list.length == 0) {
+                setImageUrl("logo512.png");
+            } else {
+                const itemRef = list[0];
+                getDownloadURL(itemRef)
+                    .then((url) => {
+                        setImageUrl(url);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         })
         .catch((error) => {
             console.log(error);
-            // Handle any errors
         });
+
+
+
 
     return (
         <></>
